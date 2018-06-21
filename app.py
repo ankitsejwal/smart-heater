5c5c127ea3d15deffb8cd357e922c63c22db36cf
ankitsejwal <ankit@sejw.al>
2018-06-22 02:53:00 +1000

    combining twilio, GPIO and flask app all together

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+import RPi.GPIO as GPIO
+from thing import PiThing
+
+pi_thing = PiThing()
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return 'Welcome'
+    message_body = request.form['Body']
+    resp = MessagingResponse()
+    
+    if message_body.lower() == 'on':
+        pi_thing.set_led(True)
+        resp.message_body('LED turned ON')
 
-
+    elif message_body.lower() == 'off' :
+        pi_thing.set_led(False)
+        resp.message_body('LED turned OFF')
+    
+    return str(resp)
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5005, debug=True)
