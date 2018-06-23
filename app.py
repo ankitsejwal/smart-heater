77a2e2760fdd04216f3609378994cbbe8c2da29b
ankitsejwal <ankit@sejw.al>
2018-06-23 18:53:46 +1000

    better commenting and code refactor

--- app.py
+++ app.py
@@ -3,9 +3,11 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 import RPi.GPIO as GPIO
-from thing import PiThing
+from heater import Heater
 
-pi_thing = PiThing()
+heater = Heater()
+
+# ON and OFF phrase that we want to match with recieved SMS
 on_phrase = ['on', 'heater on', 'turn heater on', 'turn on']
 off_phrase = ['off', 'heater off', 'turn heater off', 'turn off']
 
@@ -13,18 +15,24 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+
+    # Get message from twilio
     message_body = request.form['Body']
     resp = MessagingResponse()
     
+    # if user wants to turn heater ON
     if message_body.lower() in on_phrase:
-        pi_thing.set_led(True)
+        heater.set_led(True)
         resp.message('LED turned ON')
 
+    # if user wants to turn heater OFF
     elif message_body.lower() in off_phrase :
-        pi_thing.set_led(False)
+        heater.set_led(False)
         resp.message('LED turned OFF')
     
     return str(resp)
     
 if __name__ == '__main__':
+    # host 0.0.0.0 allows you to access your raspi from a web browser of
+    # other computers on the same network http://0.0.0.0:5005
     app.run(host='0.0.0.0', port=5005, debug=True)
